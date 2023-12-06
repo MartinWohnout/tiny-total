b1dfe9fc702c846bde5b7fe401a0cb0ded3dff3c
Martin Wohnout <martin.wohnout@proton.me>
2023-12-06 23:25:36 +0100

    - Weitere Unittests

--- TinyTotalTests/MainTableViewModelTests.cs
+++ TinyTotalTests/MainTableViewModelTests.cs
@@ -5,16 +5,6 @@ namespace TinyTotal.UnitTests.ViewModels
         // TODO: Test, ob in die Logs geschrieben wird
 
         [Test]
-        public void AddContentCandidateCommand_AddNewLineToEmpty_HasContent()
-        {
-            const decimal InitialValue = 1.0M;
-
-            var testModel = GetTestModel();
-            testModel.ContentCandidate = InitialValue.ToString();
-            testModel.AddContentCandidateCommand.Execute(null);
-            Assert.AreEqual(testModel.HasContent, true);
-        }
-        [Test]
         public void AddContentCandidateCommand_AddNewLine_ClearsContentCandidate()
         {
             const decimal InitialValue = 1.0M;
@@ -26,6 +16,26 @@ namespace TinyTotal.UnitTests.ViewModels
         }
 
         [Test]
+        public void AddContentCandidateCommand_AddNewLineToEmpty_HasContent()
+        {
+            const decimal InitialValue = 1.0M;
+
+            var testModel = GetTestModel();
+            AddContentToModel(testModel, InitialValue.ToString());
+            Assert.AreEqual(testModel.HasContent, true);
+        }
+
+        [Test]
+        public void AddContentCandidateCommand_AddNumberToEmpty_CalculatesTotal()
+        {
+            const decimal InitialValue = 1.0M;
+
+            var testModel = GetTestModel();
+            AddContentToModel(testModel, InitialValue.ToString());
+            Assert.AreEqual(testModel.ColumnTotal, InitialValue);
+        }
+
+        [Test]
         public void AddContentCandidateCommand_AddNumber_CalculatesTotal()
         {
             const decimal InitialValue = 1.0M;
@@ -33,23 +43,48 @@ namespace TinyTotal.UnitTests.ViewModels
             Func<decimal> GetColumnTotal = () => InitialValue + AddedValue;
 
             var testModel = GetTestModel();
-            testModel.ContentCandidate = InitialValue.ToString();
-            testModel.AddContentCandidateCommand.Execute(null);
-            testModel.ContentCandidate = AddedValue.ToString();
-            testModel.AddContentCandidateCommand.Execute(null);
+            AddContentToModel(testModel, InitialValue.ToString());
+            AddContentToModel(testModel, AddedValue.ToString());
             Assert.AreEqual(testModel.ColumnTotal, GetColumnTotal());
+        }
+
+        [Test]
+        public void AddContentCandidateCommand_AddNull_NoNewLine()
+        {
+            var testModel = GetTestModel();
+            AddContentToModel(testModel, null);
+            Assert.AreEqual(testModel.HasContent, false);
         }
 
         [Test]
         public void AddContentCandidateCommand_AddEmptyValue_NoNewLine()
         {
-            throw new NotImplementedException();
+            var testModel = GetTestModel();
+            AddContentToModel(testModel, string.Empty);
+            Assert.AreEqual(testModel.HasContent, false);
         }
 
         [Test]
         public void AddContentCandidateCommand_AddText_DisplaysText()
         {
-            throw new NotImplementedException();
+            const string DisplayText = "TestContent";
+
+            var testModel = GetTestModel();
+            AddContentToModel(testModel, DisplayText);
+            var displayedLine = testModel.DataColumn.First();
+            Assert.AreEqual(displayedLine.DisplayValue, DisplayText);
+        }
+
+        [Test]
+        public void AddContentCandidateCommand_AddText_UnchangedTotal()
+        {
+            const decimal NumericValue = 1.0M;
+            const string TextValue = "TestContent";
+
+            var testModel = GetTestModel();
+            AddContentToModel(testModel, NumericValue.ToString());
+            AddContentToModel(testModel, TextValue);
+            Assert.AreEqual(testModel.ColumnTotal, NumericValue);
         }
 
         [Test]
@@ -63,6 +98,12 @@ namespace TinyTotal.UnitTests.ViewModels
         private MainTableViewModel GetTestModel()
         {
             return new MainTableViewModel();
+        }
+
+        private void AddContentToModel(MainTableViewModel targetModel, string content)
+        {
+            targetModel.ContentCandidate = content;
+            targetModel.AddContentCandidateCommand.Execute(null);
         }
     }
 }
